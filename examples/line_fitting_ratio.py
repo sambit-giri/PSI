c1f70295e207649e4034701c6e0da30cee2bb515
Sambit Giri <sambitgiri@Sambits-MacBook-Pro.local>
2020-05-15 19:41:17 +0200

    distance function removed

--- examples/line_fitting_ratio.py
+++ examples/line_fitting_ratio.py
@@ -10,24 +10,24 @@ y_obs = line.observation()
 ## LFIRE
 # 1 param
 simulator = lambda x: line.simulator(x, line.true_intercept)
-distance  = psi.distances.euclidean
+#distance  = psi.distances.euclidean
 
 prior  = {'m': 'uniform'}#, 'c': 'uniform'}
 bounds = {'m': [-2.5, 0.5]}#, 'c': [0,10]}
 
-lfi = psi.LFIRE(simulator, distance, y_obs, prior, bounds, sim_out_den=None, n_m=100, n_theta=100, n_grid_out=100, thetas=None)
+lfi = psi.LFIRE(simulator, y_obs, prior, bounds, sim_out_den=None, n_m=100, n_theta=100, n_grid_out=100, thetas=None)
 lfi.run()
 
 plt.plot(lfi.thetas, lfi.posterior)
 
 # 2 param
 simulator2 = lambda x: line.simulator(x[0], x[1])
-distance2  = psi.distances.euclidean
+#distance2  = psi.distances.euclidean
 
 prior2  = {'m': 'uniform', 'c': 'uniform'}
 bounds2 = {'m': [-2.5, 0.5], 'c': [0,10]}
 
-lfi2 = psi.LFIRE(simulator2, distance2, y_obs, prior2, bounds2, sim_out_den=None, n_m=100, n_theta=100, n_grid_out=100, thetas=None)
+lfi2 = psi.LFIRE(simulator2, y_obs, prior2, bounds2, sim_out_den=None, n_m=100, n_theta=100, n_grid_out=100, thetas=None)
 lfi2.run()
 
 plt.scatter(lfi2.thetas[:,0], lfi2.thetas[:,1], c=lfi2.posterior)
