4ed61076d217ba0c42bc3883a10aa359dced62d4
Sambit Giri <sambitgiri@Sambits-MacBook-Pro.local>
2020-06-19 18:11:55 +0200

    edit

--- psi/bolfi.py
+++ psi/bolfi.py
@@ -114,7 +114,7 @@ class BOLFI_1param:
 			condition2 = self.successive_JS_dist[-1]<self.successive_JS_tol
 
 class BOLFI:
-	def __init__(self, simulator, distance, observation, prior, bounds, N_init=5, gpr=None, max_iter=100, cv_JS_tol=0.01, successive_JS_tol=0.01, n_grid_out=100, exploitation_exploration=None, sigma_tol=0.001, inside_nSphere=True, fill_value=100):
+	def __init__(self, simulator, distance, observation, prior, bounds, N_init=5, gpr=None, max_iter=100, cv_JS_tol=0.01, successive_JS_tol=0.01, n_grid_out=100, exploitation_exploration=None, sigma_tol=0.001, inside_nSphere=False, fill_value=np.nan):
 		self.N_init  = N_init
 		self.gpr = GaussianProcessRegressor() if gpr is None else gpr
 		self.simulator = simulator
@@ -164,7 +164,7 @@ class BOLFI:
 		for train_index, test_index in kf.split(X):
 			X_train, X_test = X[train_index], X[test_index]
 			y_train, y_test = y[train_index], y[test_index]
-			self.gpr.fit(X_train, y_train)
+			self.gpr.fit(X_train[np.isfinite(y_train)], y_train[np.isfinite(y_train)])
 			y_pred, y_std = self.gpr.predict(self.xout, return_std=True)
 			unnorm_post_mean = np.exp(-y_pred/2.)
 			pdfs.append(unnorm_post_mean)
@@ -207,7 +207,7 @@ class BOLFI:
 			if self.sigma_tol is not None:
 				self.exploitation_exploration = 1./self.sigma_tol if np.any(self.sigma_theta>self.sigma_tol) else 1.
 			#X_next = bopt.propose_location(bopt.expected_improvement, self._adjust_shape(self.params), self.posterior_params, self.gpr, self.lfi.bounds, n_restarts=10).T
-			X_next = bopt.propose_location(bopt.negativeGP_LCB, X, y, self.gpr, self.bounds, n_restarts=10, xi=self.exploitation_exploration).T
+			X_next = bopt.propose_location(bopt.negativeGP_LCB, X[np.isfinite(y)], y[np.isfinite(y)], self.gpr, self.bounds, n_restarts=10, xi=self.exploitation_exploration).T
 
 			#y_next = self._simulator(X_next.T)
 			#d_next = self.distance(self.y_obs, y_next)
@@ -228,5 +228,125 @@ class BOLFI:
 			print('\nFinal training of GPR for output.')
 			X = self.params.reshape(-1,1) if self.params.ndim==1 else self.params
 			y = self.dists.reshape(-1,1) if self.dists.ndim==1 else self.dists
-			self.gpr.fit(X, y)
+			self.gpr.fit(X[np.isfinite(y)], y[np.isfinite(y)])
+
+
+class BOLFI_postGPR:
+	def __init__(self, simulator, distance, observation, prior, bounds, N_init=5, gpr=None, max_iter=100, cv_JS_tol=0.01, successive_JS_tol=0.01, n_grid_out=100, exploitation_exploration=None, sigma_tol=0.001, inside_nSphere=True, fill_value=1000):
+		self.N_init  = N_init
+		self.gpr = GaussianProcessRegressor() if gpr is None else gpr
+		self.simulator = simulator
+		self.distance  = distance
+		self.y_obs = observation
+		self.param_names = [kk for kk in prior]
+		self.param_bound = bounds
+		self.bounds = np.array([bounds[kk] for kk in self.param_names])
+		self.bound_mins = self.bounds.min(axis=1)
+		self.bound_maxs = self.bounds.max(axis=1)
+		#self.sample_prior = {}
+		#for i,kk in enumerate(self.param_names):
+		#	self.sample_prior[kk] = lambda: bounds[kk][0]+(bounds[kk][1]-bounds[kk][0])*np.random.uniform()
+		self.xout = _grid_bounds(self.bounds, n_grid=n_grid_out)
+		self.max_iter = max_iter
+		self.params = np.array([])
+		self.post_mean_unnorm  = []
+		self.post_mean_normmax = []
+		self.cv_JS_tol  = cv_JS_tol
+		self.cv_JS_dist = {'mean':[], 'std':[]}
+		self.successive_JS_tol  = successive_JS_tol
+		self.successive_JS_dist = []	
+
+		self.exploitation_exploration = exploitation_exploration
+		self.sigma_tol = sigma_tol	
+		self.inside_nSphere = inside_nSphere
+		self.fill_value = fill_value
+
+	def sample_prior(self, kk):
+		return self.param_bound[kk][0]+(self.param_bound[kk][1]-self.param_bound[kk][0])*np.random.uniform()
+
+	def sim_n_dist(self, xi):
+		if self.inside_nSphere:
+			xr = np.sum(((xi-self.bound_mins)/(self.bound_maxs-self.bound_mins)-0.5)**2)
+			if xr>0.25: return self.fill_value
+		yi = self.simulator(xi)
+		di = self.distance(self.y_obs, yi)
+		return di
+
+	def fit_model(self, params, dists):
+		X = params.reshape(-1,1) if params.ndim==1 else params
+		y = dists.reshape(-1,1) if dists.ndim==1 else dists
+
+		n_cv = 10 if y.size>50 else 5
+		kf = KFold(n_splits=n_cv)
+		pdfs = []
+		for train_index, test_index in kf.split(X):
+			X_train, X_test = X[train_index], X[test_index]
+			y_train, y_test = y[train_index], y[test_index]
+			self.gpr.fit(X_train, np.exp(-y_train/2.))
+			y_pred, y_std = self.gpr.predict(self.xout, return_std=True)
+			unnorm_post_mean = y_pred
+			pdfs.append(unnorm_post_mean)
+		
+		cvdist = np.array([distances.jensenshannon(p1,p2) for p1 in pdfs for p2 in pdfs])
+		self.cv_JS_dist['std'].append(cvdist.std())
+		self.cv_JS_dist['mean'].append(cvdist.mean())
+
+		self.gpr.fit(X, np.exp(-y/2.))
+		y_pred, y_std = self.gpr.predict(self.xout, return_std=True)
+		unnorm_post_mean = y_pred
+		norm_post_mean   = unnorm_post_mean/unnorm_post_mean.max()
+		norm_post_std    = y_std.flatten()
+		self.sigma_theta = norm_post_std
+		self.post_mean_unnorm.append(unnorm_post_mean)
+		self.post_mean_normmax.append(norm_post_mean)
+		return self.cv_JS_dist['mean'][-1] #cvdist.std()
+
+	def run(self, max_iter=None, trained_gpr=True):
+		if max_iter is not None: self.max_iter = max_iter
+		#gpr = self.gpr
+		start_iter = self.params.size
+		# Initialization
+		if start_iter<self.N_init:
+			params  = np.array([[self.sample_prior(kk) for kk in self.param_names] for i in range(self.N_init)]).squeeze()
+			#sim_out = np.array([self._simulator(i) for i in params])
+			#dists   = np.array([self.distance(self.y_obs, ss) for ss in sim_out])
+			dists = np.array([self.sim_n_dist(i) for i in params])
+			self.params = params
+			self.dists  = dists
+			msg = self.fit_model(self.params, self.dists)
+			hf.loading_verbose('{0:.6f}'.format(msg))
+		
+		# Further sampling
+		start_iter = len(self.params)
+		condition1, condition2 = False, False
+		for n_iter in range(start_iter,self.max_iter):
+			if condition1 and condition2: break
+			X = self.params.reshape(-1,1) if self.params.ndim==1 else self.params
+			y = self.dists.reshape(-1,1) if self.dists.ndim==1 else self.dists
+
+			if self.sigma_tol is not None:
+				self.exploitation_exploration = 1./self.sigma_tol if np.any(self.sigma_theta>self.sigma_tol) else 1.
+			#X_next = bopt.propose_location(bopt.expected_improvement, self._adjust_shape(self.params), self.posterior_params, self.gpr, self.lfi.bounds, n_restarts=10).T
+			X_next = bopt.propose_location(bopt.negativeGP_LCB, X, np.exp(-y/2.), self.gpr, self.bounds, n_restarts=10, xi=self.exploitation_exploration).T
+
+			#y_next = self._simulator(X_next.T)
+			#d_next = self.distance(self.y_obs, y_next)
+			d_next = self.sim_n_dist(X_next.T)
+
+			self.params = np.append(self.params, X_next, axis=0) 
+			self.dists  = np.append(self.dists, d_next)
+			sucJSdist   = distances.jensenshannon(self.post_mean_normmax[-1], self.post_mean_normmax[-2])[0] if len(self.post_mean_normmax)>1 else 10
+			self.successive_JS_dist.append(sucJSdist)
+
+			msg = self.fit_model(self.params, self.dists)
+			hf.loading_verbose('{0:6d}|{1:.6f}|{1:.6f}'.format(n_iter+1,msg,sucJSdist))
+			#condition1 = self.cv_JS_dist['mean'][-1]+self.cv_JS_dist['std'][-1]<self.cv_JS_tol
+			condition1 = self.cv_JS_dist['mean'][-1]<self.cv_JS_tol
+			condition2 = self.successive_JS_dist[-1]<self.successive_JS_tol
+
+		if trained_gpr:
+			print('\nFinal training of GPR for output.')
+			X = self.params.reshape(-1,1) if self.params.ndim==1 else self.params
+			y = self.dists.reshape(-1,1) if self.dists.ndim==1 else self.dists
+			self.gpr.fit(X, np.exp(-y/2.))
 
